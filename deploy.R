a002bddace70674e83e23d7adfa5878faf3325e0
Lisa Anders <lisa.anders@rstudio.com>
2022-10-05 23:48:14 +0000

    Syncing changes (simplifying deploy.R)

--- deploy.R
+++ deploy.R
@@ -13,21 +13,7 @@
 # During app deployment you can see the app bundle number, save this. Alternatively you can access this from Connect under `Information` as the `Content ID`. 
 # The app bundle is: 12929 
 
-# This will "auto-detect" if it has been deployed before, meaning that it depends on having the manifest.json file and rsconnect directory with the correct information
 
-#################################### FIRST
-library(rsconnect)
-
-rsconnect::writeManifest()
-
-rsconnect::deployApp(
-  appDir = getwd(),
-  #appFiles = NULL,
-  account = "lisa.anders",
-  server = "colorado.rstudio.com"
-)
-
-#################################### SECOND
 # This will attempt to deploy to the defined appId. If the content types don't match (for example, overwriting a shiny app with a static rmarkdown), then it will throw an error. User will be prompted for whether or not they want to overwrite the existing content in the Console window. 
 
 library(rsconnect)
@@ -37,45 +23,8 @@ rsconnect::writeManifest()
 rsconnect::deployApp(
   appDir = getwd(),
   appId = "12929",
-  #account = "lisa.anders",
   server = "colorado.rstudio.com",
   forceUpdate = TRUE
 )
-
-
-#################################### THIRD
-# We can also run this without needing user inputs by forcing the content to be overwritten without prompting with forceUpdate = TRUE and by authenticating to the server using an API rather than through the GUI. 
-
-# addServer("https://colorado.rstudio.com/rsc/__api__", "myserver")
-addConnectServer("https://colorado.rstudio.com/rsc/", "myserver")
-
-# Connecting account via API key
-rsconnect::connectApiUser(
-  account = "lisa.anders",
-  server = "myserver",
-  apiKey = Sys.getenv("CONNECT_API_KEY"),
-  forceUpdate = TRUE
-)
-
-message("Please wait, publishing")
-
-rsconnect::deployApp(
-  appDir = getwd(),
-  appId = "12929",
-  forceUpdate = TRUE
-)
-
-#################################### FOURTH
-# Instead of publishing we can trigger re-renders and other programmatic options with the [connectapi package](https://github.com/rstudio/connectapi)
-
-library(connectapi)
-client <- connectapi::connect(
-  host = 'https://colorado.rstudio.com/rsc/',
-  api_key = Sys.getenv("CONNECT_API_KEY")
-)
-
-# Refer to: https://rstudio.github.io/connectwidgets/
-
-
 
 
